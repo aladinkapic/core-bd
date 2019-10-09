d2fd39599afec7ccb783d56ce735e6c84be654bc
aladin.kapic <aladin.kapic@teneo.ba>
2019-10-09 16:22:13 +0200

    upraznjena radna

--- resources/views/ostalo/interno_trziste/pregled.blade.php
+++ resources/views/ostalo/interno_trziste/pregled.blade.php
@@ -15,86 +15,110 @@
         @include('ostalo/interno_trziste/fajlovi/menu')
         @include('ostalo/interno_trziste/fajlovi/forma')
 
-        @include('template.snippets.filters', ['var'  => $planovi])
+        @include('template.snippets.filters', ['var'  => $radnaMjesta])
 
 
-        <table class="table table-bordered" id="filtering">
+        <table class="table table-bordered table-sm" id="filtering">
             <thead>
             <tr>
                 @include('template.snippets.filters_header')
-                @if(!isset($prekobrojni))
-                    <th scope="col" class="text-center">Rješenje</th>
-                @endif
-                <th scope="col" class="text-center">Pregled</th>
+{{--                @if(!isset($prekobrojni))--}}
+{{--                    <th scope="col" class="text-center">Rješenje</th>--}}
+{{--                @endif--}}
+                <th width="120px" class="text-center">Akcije</th>
             </tr>
             </thead>
             <tbody>
 
             @php $counter = 1; @endphp
-            @foreach($planovi as $plan)
-                @foreach($plan->organizacioneJedinice as $orgJedinica)
-                    @foreach($orgJedinica->radnaMjesta as $radnoMjesto)
-                        @if(isset($prekobrojni))
-                            @if($radnoMjesto->broj_izvrsilaca < $radnoMjesto->sluzbenici->count())
-                                <tr>
-                                    <td>{{$counter++}}</td>
-                                    <td>
-                                        {{$radnoMjesto->naziv_rm ?? '/'}}
-                                    </td>
-                                    <td>
-                                        {{$orgJedinica->naziv ?? '/'}}
-                                    </td>
-                                    <td>
-                                        {{$radnoMjesto->sifra_rm ?? '/'}}
-                                    </td>
-                                    <td>
-                                        {{$radnoMjesto->broj_izvrsilaca ?? '/'}}
-                                    </td>
-                                    <td>
-                                        {{$radnoMjesto->sluzbenici->count() ?? '/'}}
-                                    </td>
-                                    <td class="text-center">
-                                        <a href="{{route('internotrziste.sviprekobrojniljudi', ['id' => $radnoMjesto->id])}}"
-                                           title="Pregled svih službenika na radnom mjestu">
-                                            <i class="fa fa-eye" style="margin-left:10px;"></i> Pregled
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endif
-                        @elseif($radnoMjesto->broj_izvrsilaca >= $radnoMjesto->sluzbenici->count())
-                            <tr>
-                                <td>
-                                    {{$radnoMjesto->naziv_rm ?? '/'}}
-                                </td>
-                                <td>
-                                    {{$orgJedinica->naziv ?? '/'}}
-                                </td>
-                                <td>
-                                    {{$radnoMjesto->sifra_rm ?? '/'}}
-                                </td>
-                                <td>
-                                    {{$radnoMjesto->broj_izvrsilaca ?? '/'}}
-                                </td>
-                                <td>
-                                    {{$radnoMjesto->sluzbenici->count() ?? '/'}}
-                                </td>
-                                <td class="text-center">
-                                    <a href="#" title="Dodajte / uredite rješenje" class="rjesenje"
-                                       data-id="{{ $radnoMjesto->id ?? '1'}}" data-name="{{ $radnoMjesto->naziv_rm ?? '/'}}">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                </td>
-                                <td class="text-center">
-                                    <a href="{{route('radnamjesta.rjesenje', ['id' => $radnoMjesto->id, 'what' => 'true'])}}"
-                                       title="Pregledajte radno mjesto">
-                                        <i class="fa fa-eye" style="margin-left:10px;"></i>
-                                    </a>
-                                </td>
-                            </tr>
-                        @endif
-                    @endforeach
-                @endforeach
+
+            @foreach($radnaMjesta as $rm)
+                @if($rm->broj_izvrsilaca > $rm->sluzbeniciRel->count())
+                    <tr>
+                        <td>{{$counter++}}</td>
+                        <td>{{$rm->naziv_rm}}</td>
+                        <td>{{$rm->orgjed->naziv }}</td>
+                        <td>{{$rm->sifra_rm ?? '/'}}</td>
+                        <td>{{$rm->broj_izvrsilaca ?? '/'}}</td>
+                        <td>{{$rm->sluzbeniciRel->count()}}</td>
+                        <td class="text-center">
+                            <a href="#" title="Dodajte / uredite rješenje" class="rjesenje" data-id="{{ $rm->id ?? '1'}}" data-name="{{ $rm->naziv_rm ?? '/'}}">
+                                <i class="fas fa-edit"></i>
+                            </a>
+                            <a href="{{route('radnamjesta.rjesenje', ['id' => $rm->id, 'what' => 'true'])}}"
+                               title="Pregledajte radno mjesto">
+                                <i class="fa fa-eye" style="margin-left:10px;"></i>
+                            </a>
+                        </td>
+                    </tr>
+                @endif
             @endforeach
+
+
+{{--            @foreach($planovi as $plan)--}}
+{{--                @foreach($plan->organizacioneJedinice as $orgJedinica)--}}
+{{--                    @foreach($orgJedinica->radnaMjesta as $radnoMjesto)--}}
+{{--                        @if(isset($prekobrojni))--}}
+{{--                            @if($radnoMjesto->broj_izvrsilaca < $radnoMjesto->sluzbenici->count())--}}
+{{--                                <tr>--}}
+{{--                                    <td>{{$counter++}}</td>--}}
+{{--                                    <td>--}}
+{{--                                        {{$radnoMjesto->naziv_rm ?? '/'}}--}}
+{{--                                    </td>--}}
+{{--                                    <td>--}}
+{{--                                        {{$orgJedinica->naziv ?? '/'}}--}}
+{{--                                    </td>--}}
+{{--                                    <td>--}}
+{{--                                        {{$radnoMjesto->sifra_rm ?? '/'}}--}}
+{{--                                    </td>--}}
+{{--                                    <td>--}}
+{{--                                        {{$radnoMjesto->broj_izvrsilaca ?? '/'}}--}}
+{{--                                    </td>--}}
+{{--                                    <td>--}}
+{{--                                        {{$radnoMjesto->sluzbenici->count() ?? '/'}}--}}
+{{--                                    </td>--}}
+{{--                                    <td class="text-center">--}}
+{{--                                        <a href="{{route('internotrziste.sviprekobrojniljudi', ['id' => $radnoMjesto->id])}}"--}}
+{{--                                           title="Pregled svih službenika na radnom mjestu">--}}
+{{--                                            <i class="fa fa-eye" style="margin-left:10px;"></i> Pregled--}}
+{{--                                        </a>--}}
+{{--                                    </td>--}}
+{{--                                </tr>--}}
+{{--                            @endif--}}
+{{--                        @elseif($radnoMjesto->broj_izvrsilaca >= $radnoMjesto->sluzbenici->count())--}}
+{{--                            <tr>--}}
+{{--                                <td>--}}
+{{--                                    {{$radnoMjesto->naziv_rm ?? '/'}}--}}
+{{--                                </td>--}}
+{{--                                <td>--}}
+{{--                                    {{$orgJedinica->naziv ?? '/'}}--}}
+{{--                                </td>--}}
+{{--                                <td>--}}
+{{--                                    {{$radnoMjesto->sifra_rm ?? '/'}}--}}
+{{--                                </td>--}}
+{{--                                <td>--}}
+{{--                                    {{$radnoMjesto->broj_izvrsilaca ?? '/'}}--}}
+{{--                                </td>--}}
+{{--                                <td>--}}
+{{--                                    {{$radnoMjesto->sluzbenici->count() ?? '/'}}--}}
+{{--                                </td>--}}
+{{--                                <td class="text-center">--}}
+{{--                                    <a href="#" title="Dodajte / uredite rješenje" class="rjesenje"--}}
+{{--                                       data-id="{{ $radnoMjesto->id ?? '1'}}" data-name="{{ $radnoMjesto->naziv_rm ?? '/'}}">--}}
+{{--                                        <i class="fas fa-edit"></i>--}}
+{{--                                    </a>--}}
+{{--                                </td>--}}
+{{--                                <td class="text-center">--}}
+{{--                                    <a href="{{route('radnamjesta.rjesenje', ['id' => $radnoMjesto->id, 'what' => 'true'])}}"--}}
+{{--                                       title="Pregledajte radno mjesto">--}}
+{{--                                        <i class="fa fa-eye" style="margin-left:10px;"></i>--}}
+{{--                                    </a>--}}
+{{--                                </td>--}}
+{{--                            </tr>--}}
+{{--                        @endif--}}
+{{--                    @endforeach--}}
+{{--                @endforeach--}}
+{{--            @endforeach--}}
             </tbody>
         </table>
     </div>
