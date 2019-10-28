384b73dc26e49c064d4ab338ddbd733d5ac004f2
aladin.kapic <aladin.kapic@teneo.ba>
2019-10-28 09:33:41 +0100

    without orange

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -17,17 +17,12 @@
         @yield('other_css_links')
     </head>
     <body>
-<<<<<<< HEAD
-        <div class="alert alert-fill-success mb-0" style="background: #e9681c; border-radius: 0px;">
-            <div class="container">
-                <i class="fa fa-exclamation-triangle"></i> {{__('Poštovani korisnici. Sistem je u fazi finalizacije te je moguće da primijetite funkcionalnosti koje nisu potpune ili se ne ponašaju shodno pretpostavljenom. Molimo Vas da')}} <a href="{{ route('feedback.index') }}" class="text-white font-weight-bold">{{__('ovdje')}}</a> {{__('prijavite Vaše komentare.')}}
-=======
         @if(!isset($withoutMenu))
             <div class="alert alert-fill-success mb-0" style="background: #e9681c; border-radius: 0px;">
                 <div class="container">
                     <i class="fa fa-exclamation-triangle"></i> Poštovani korisnici. Sistem je u fazi finalizacije te je moguće da primijetite funkcionalnosti koje nisu potpune ili se ne ponašaju shodno pretpostavljenom. Molimo Vas da <a href="{{ route('feedback.index') }}" class="text-white font-weight-bold">ovdje</a> prijavite Vaše komentare.
                 </div>
->>>>>>> 2ae968b90de625a102e204988b7aa8d78656efbd
+
             </div>
         @endif
         <div class="unload">
