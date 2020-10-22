7e5c5cee2843e4ed0d4ca7f22175d9ad7e3a36ff
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2020-10-22 14:28:19 +0200

    Working time

--- app/Console/Commands/WorkTime.php
+++ app/Console/Commands/WorkTime.php
@@ -33,9 +33,11 @@ class WorkTime extends Command{
                         $total = $datum_od->diffInDays($datum_do);
                         $total = (int)($total * $zasnivanje->koeficijent) / 100;
 
-                        $years  = (int)($total / 360);
+                        $years  = (int)($total / 365);
                         $months = (int)(($total - ($years * 365)) / 30);
                         $days   = (int)(($total - ($years * 365) - ($months * 30)));
+
+                        dd($years, $months, $days);
 
                         $zasnivanje->update([
                             'obracunati_r_s_god' => $years,
