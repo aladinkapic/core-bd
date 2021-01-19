9b218d81eadb53d4707c75e422fd6a48f0ed8871
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2021-01-19 14:31:55 +0100

    ticket #124034 -- c2s2

--- app/Http/Controllers/SluzbenikController.php
+++ app/Http/Controllers/SluzbenikController.php
@@ -513,7 +513,7 @@ class SluzbenikController extends Controller
             'sluzbenikRel.rm.stepenSS.name' => 'Stepen stručne spreme - radno mjesto',
             'privremeniRel.privremeno_mjesto.naziv_rm' => 'Privremeni premještaj',
             'radnoMjesto.orgjed.naziv' => 'Organizaciona jedinica',
-            'radnoMjesto.orgjed.organizacija.organ.naziv' => 'Organ javne uprave',
+            'sluzbenikRel.rm.orgjed.organizacija.organ.naziv' => 'Organ javne uprave',
             'prebivaliste.adresa_prebivalista' => 'Adresa boravišta',
             'prebivaliste.mjesto_prebivalista' => 'Mjesto prebivališta',
             'prebivaliste.adresa_boravista' => 'Adresa prebivališta',
